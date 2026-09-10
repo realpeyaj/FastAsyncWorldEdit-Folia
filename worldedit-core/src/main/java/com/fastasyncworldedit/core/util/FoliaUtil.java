0222db4f168d3bb5ff64d7b001ac086825d8e670
realpeyaj <princejohnguadz271@gmail.com>
2026-09-10 19:20:50 +0800

    docs: add @since TODO javadoc annotations to FoliaUtil

--- worldedit-core/src/main/java/com/fastasyncworldedit/core/util/FoliaUtil.java
+++ worldedit-core/src/main/java/com/fastasyncworldedit/core/util/FoliaUtil.java
@@ -1,13 +1,30 @@
 package com.fastasyncworldedit.core.util;
 
+/**
+ * Utility for detecting whether the runtime server environment is Folia.
+ *
+ * @since TODO
+ */
 public class FoliaUtil {
 
     private static final Boolean FOLIA_DETECTED = detectFolia();
 
+    /**
+     * Checks if the server is running on Folia.
+     *
+     * @return true if running on Folia
+     * @since TODO
+     */
     public static boolean isFoliaServer() {
         return FOLIA_DETECTED;
     }
 
+    /**
+     * Checks if the server is running on Folia.
+     *
+     * @return true if running on Folia
+     * @since TODO
+     */
     public static boolean isFolia() {
         return FOLIA_DETECTED;
     }
